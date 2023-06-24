a71c69d1edc0875612d98fc851fbd4e9473485f6
Auri <Atalonica@users.noreply.github.com>
2023-06-24 18:11:57 +0200

    Change AEAD algorithm to ASCON128 (NIST finalist)

--- SecureRF.h
+++ SecureRF.h
@@ -1,7 +1,7 @@
 
 /*
- * Copyright (C) 2021 Atalonica.
- * Copyright (C) 2021 Southern Storm Software, Pty Ltd.
+ * Copyright (C) 2023 Atalonica.
+ * Copyright (C) 2023 Southern Storm Software, Pty Ltd.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -25,127 +25,49 @@
 #ifndef SECURERF_H
 #define SECURERF_H
 
-/* Xoodyak authenticated encryption algorithm intended to be used with RFM69
+/* ASCON128 authenticated encryption algorithm intended to be used with RFM69
  * Adds time-based nonce expiration
- * References:  https://keccak.team/xoodyak.html
- *              https://github.com/rweather/lwc-finalists
+ * References:  https://ascon.iaik.tugraz.at/
+ *              https://github.com/rweather/ascon-suite/
  */
 
 /*********** Includes ***********/
 #include <Arduino.h>
 
 /*********** Definitions ***********/
-#define XOODYAK_KEY_SIZE 16       /* Size of the key for Xoodyak                */
-#define XOODYAK_TAG_SIZE 16       /* Size of the authentication tag for Xoodyak */
-#define XOODYAK_NONCE_SIZE 16     /* Size of the nonce for Xoodyak              */
-#define XOODYAK_HASH_SIZE 4       /* ize of the hash output for Xoodyak         */
-#define RFM69_MAX_PAYLOAD_SIZE 61 /* Max size for RFM69 payload in bytes        */
-#define MAX_AD_SIZE 4             /* Max associated data size in bytes          */
-#define NONCE_LIFETIME 500        /* Max nonce life time (in ms) (keep small)   */
-#define NONCE_MIN_GEN_TIME 1000   /* Min allowed nonce generation time (in ms)  */
-
-#define XOODYAK_ABSORB_RATE 44  /* Absorbing data rate into the sponge state                            */
-#define XOODYAK_HASH_RATE 16    /* Hash absorbing data rate (hashing mode)                              */
-#define XOODYAK_SQUEEZE_RATE 24 /* Rate for squeezing data out of the sponge                            */
-#define XOODYAK_PHASE_UP 0      /* Indicates block permutation has just been performed                  */
-#define XOODYAK_PHASE_DOWN 1    /* Indicates data has been absorbed but block permutation has not been  */
-#define XOODYAK_HASH_MODE_INIT_ABSORB 0
-#define XOODYAK_HASH_MODE_ABSORB 1
-#define XOODYAK_HASH_MODE_SQUEEZE 2
-
-/* Generic left rotate */
-#define leftRotate(a, bits)                               \
-    (__extension__(                                       \
-        {                                                 \
-            uint32_t _temp = (a);                         \
-            (_temp << (bits)) | (_temp >> (32 - (bits))); \
-        }))
-
-#define leftRotate1(a) (leftRotate((a), 1))
-#define leftRotate5(a) (leftRotate((a), 5))
-#define leftRotate8(a) (leftRotate((a), 8))
-#define leftRotate11(a) (leftRotate((a), 11))
-#define leftRotate14(a) (leftRotate((a), 14))
-
-/* XOR a source byte buffer against a destination */
-#define lw_xor_block(dest, src, len)       \
-    do                                     \
-    {                                      \
-        unsigned char *_dest = (dest);     \
-        const unsigned char *_src = (src); \
-        unsigned _len = (len);             \
-        while (_len > 0)                   \
-        {                                  \
-            *_dest++ ^= *_src++;           \
-            --_len;                        \
-        }                                  \
-    } while (0)
-
-/* XOR a source byte buffer against a destination and write to another
- * destination at the same time. */
-#define lw_xor_block_swap(dest2, dest, src, len) \
-    do                                           \
-    {                                            \
-        unsigned char *_dest2 = (dest2);         \
-        unsigned char *_dest = (dest);           \
-        const unsigned char *_src = (src);       \
-        unsigned _len = (len);                   \
-        while (_len > 0)                         \
-        {                                        \
-            unsigned char _temp = *_src++;       \
-            *_dest2++ = *_dest ^ _temp;          \
-            *_dest++ = _temp;                    \
-            --_len;                              \
-        }                                        \
-    } while (0)
-
-/* XOR a source byte buffer against a destination and write to another
- * destination at the same time */
-#define lw_xor_block_2_dest(dest2, dest, src, len) \
-    do                                             \
-    {                                              \
-        unsigned char *_dest2 = (dest2);           \
-        unsigned char *_dest = (dest);             \
-        const unsigned char *_src = (src);         \
-        unsigned _len = (len);                     \
-        while (_len > 0)                           \
-        {                                          \
-            *_dest2++ = (*_dest++ ^= *_src++);     \
-            --_len;                                \
-        }                                          \
-    } while (0)
-
-#define xoodoo_hash_permute(state) \
-    xoodoo_permute((xoodoo_state_t *)((state)->s.state))
+#define ASCON128_KEY_SIZE 16      /* Size of the key for ASCON128                */
+#define ASCON128_TAG_SIZE 16      /* Size of the authentication tag for ASCON128 */
+#define ASCON128_NONCE_SIZE 16    /* Size of the nonce for ASCON128              */
+#define ASCON_HASH_SIZE 4        /* Size of the hash output for ASCON           */
+#define ASCON_XOF_RATE 8
+#define RFM69_MAX_PAYLOAD_SIZE 61 /* Max size for RFM69 payload in bytes         */
+#define MAX_AD_SIZE 4             /* Max associated data size in bytes           */
+#define NONCE_LIFETIME 1000       /* Max nonce life time (in ms) (keep small)    */
+#define NONCE_MIN_GEN_TIME 500    /* Min allowed nonce generation time (in ms)   */
 
 /*********** Typedef structs ***********/
 
-/* State information for the Xoodoo permutation */
+/* Structure of the internal state of the ASCON permutation. */
 typedef union
 {
-    /** Words of the state organized into rows and columns */
-    uint32_t S[3][4];
-    /** Words of the state as a single linear array */
-    uint32_t W[12];
-    /** Bytes of the state */
-    uint8_t B[12 * sizeof(uint32_t)];
+    uint64_t S[5];                  /**< 64-bit words of the state */
+    uint32_t W[10];                 /**< 32-bit words of the state */
+    uint8_t B[40];                  /**< Bytes of the state */
+    void *P[40 / sizeof(void *)];   /**< Private backend state */
 
-} xoodoo_state_t;
+} ascon_state_t;
 
-/**
- * \brief State information for Xoodyak incremental hashing modes.
- */
-typedef union
+typedef struct
 {
-    struct
-    {
-        uint8_t state[48];    /**< Current hash state */
-        uint8_t count;        /**< Number of bytes in the current block */
-        uint8_t mode;         /**< Hash mode: absorb or squeeze */
-    } s;                      /**< State */
-    unsigned long long align; /**< For alignment of this structure */
+    ascon_state_t state;    
+    unsigned char count;    
+    unsigned char mode;     
+} ascon_xof_state_t;
 
-} xoodyak_hash_state_t;
+typedef struct
+{
+    ascon_xof_state_t xof;  
+} ascon_hash_state_t;
 
 class SecureRF
 {
@@ -153,15 +75,15 @@ class SecureRF
 public:
     SecureRF();
 
-    static unsigned char PLAINTEXT[RFM69_MAX_PAYLOAD_SIZE - 1 - XOODYAK_TAG_SIZE + 1];
+    static unsigned char PLAINTEXT[RFM69_MAX_PAYLOAD_SIZE - 1 - ASCON128_TAG_SIZE + 1];
     static unsigned char ASSOCIATED[MAX_AD_SIZE + 1];
     static unsigned char SECURE_PAYLOAD[RFM69_MAX_PAYLOAD_SIZE + 1];
 
     static uint8_t PLAINTEXT_LEN, ASSOCIATED_LEN, SECURE_PAYLOAD_LEN;
 
     /* [setKeys]
-     * kx (16): input buffer with PSK key used for Xoodyak AEAD
-     * kh (16): input buffer with PSK key used for Xoodyak hashing
+     * kx (16): input buffer with PSK key used for ASCON128 AEAD
+     * kh (16): input buffer with PSK key used for ASCON128 hashing
      */
     void setKeys(const unsigned char *kx, const unsigned char *kh);
 
@@ -198,14 +120,14 @@ public:
 
 private:
     /* Parameters */
-    unsigned char keyX[XOODYAK_KEY_SIZE];
-    unsigned char keyH[XOODYAK_KEY_SIZE];
+    unsigned char keyX[ASCON128_KEY_SIZE];
+    unsigned char keyH[ASCON128_KEY_SIZE];
     bool keysSet;
     unsigned char _nReqNameId[4];
     unsigned char _nReqRandId[4];
-    unsigned char nonce[XOODYAK_NONCE_SIZE]; // nonce
+    unsigned char nonce[ASCON128_NONCE_SIZE]; // nonce
     uint32_t nonceGenTime, nonceMinGenTime = NONCE_MIN_GEN_TIME;
-    uint32_t nErrorCounter = 0;
+    uint16_t nErrorCounter = 0;
     bool incomingAEAD = false;
 
     bool setNonce(const unsigned char *n, uint8_t offset = 0);
